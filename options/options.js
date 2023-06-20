b796ead6c7e077f6a956f02ac366212bae4fbb09
Crowdbotics <engineering@crowdbotics.com>
2023-06-20 15:38:06 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "yellow-breeze-dev-93430",
+  url: "https://yellow-breeze-dev-93430.botics.co",
+  api: "https://yellow-breeze-dev-93430.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
